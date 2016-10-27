c62f3abca70a4f2a5b5ee14c7cbe16d39e9475ff
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-10-27 20:54:26 +0700

    add DormViewModel

--- Jonghor/Controllers/JonghorController.cs
+++ Jonghor/Controllers/JonghorController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Jonghor.Models;
+using Jonghor.ViewModel;
 
 namespace Jonghor.Controllers
 {
@@ -11,7 +13,8 @@ namespace Jonghor.Controllers
         // GET: Jonghor
         public ActionResult Index()
         {
-            return View();
+            DormViewModel dormViewModel = new DormViewModel();
+            return View("Homepage", dormViewModel);
         }
     }
 }
