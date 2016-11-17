79512d43062dd0f8ab61bf05b4369d7fd005cf1f
buckngaw <aon_natjaya@hotmail.com>
2016-11-17 21:44:40 +0700

    CreateValueDB

---
+++ Jonghor/Models/Dorm_LabelLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Jonghor.Models
+{
+    public class Dorm_LabelLayer
+    {
+        public List<Dorm_Label> GetDorm_Label()
+        {
+            JongHorDBEntities1 jonghor = new JongHorDBEntities1();
+
+            DbSet<Dorm_Label> dormDal = jonghor.Dorm_Label;
+
+            return dormDal.ToList<Dorm_Label>();
+        }
+    }
+}
---
+++ Jonghor/Models/Dorm_RateLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Jonghor.Models
+{
+    public class Dorm_RateLayer
+    {
+        public List<Dorm_Rate> GetDorm_Rate()
+        {
+            JongHorDBEntities1 jonghor = new JongHorDBEntities1();
+
+            DbSet<Dorm_Rate> dormDal = jonghor.Dorm_Rate;
+
+            return dormDal.ToList<Dorm_Rate>();
+        }
+    }
+}
