213bbb18711ce17b9d6f5663610c2126e2dec022
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-11-17 22:01:32 +0700

    Revert "commit"
    This reverts commit d29fd8f01118c891d1d5beef3e0621cbb3fdd42c.

---
+++ Jonghor/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jonghor.Models;
+
+namespace Jonghor.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index(string uname, string psw)
+        {
+            PersonBusinessLayer personBal = new PersonBusinessLayer();
+            List<Person> people = personBal.GetPeople();
+
+            foreach (Person person in people)
+            {
+                if(person.Name == uname)
+                {
+                    if(person.Password == psw)
+                    {
+                        return Content(uname + " login succeeded");
+                    }
+                    else
+                    {
+                        return Content("Password Incorrect");
+                    }
+                }
+            }
+            return Content("Username not found");
+            
+        }
+
+
+    }
+}
