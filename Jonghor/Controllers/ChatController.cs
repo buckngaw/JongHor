3e68cf7260b0d3a329339181c6e9b6135f6ffb68
buckngaw <aon_natjaya@hotmail.com>
2016-11-20 17:31:14 +0700

    kak

---
+++ Jonghor/Controllers/ChatController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jonghor.Models;
+
+namespace Jonghor.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index(MessageHTML m,string Checkbox)
+        {
+            switch (Checkbox)
+            {
+                case "all room":
+                    return CheckAllRoom();
+                case "specific room":
+                    return CheckSpecificRoom();
+            }
+            return new EmptyResult();
+
+        }
+
+        //send message to all room
+        public  ActionResult CheckAllRoom()
+        {
+            return View("gg");
+        }
+
+        //send message to specific room
+        public ActionResult CheckSpecificRoom()
+        {
+            return View("gg");
+        }
+    }
+}
---
+++ Jonghor/Models/MessageHTML.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jonghor.Models
+{
+    public class MessageHTML
+    {
+        public string CheckAllRoom { get; set; }
+        public string SpecificRoom { get; set; }
+        public string SubSpec { get; set; }
+        public string Message { get; set; }
+        public string BtuSubmit { get; set; }
+    }
+}
