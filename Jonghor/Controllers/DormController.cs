b8cb6256bdeef22e1740a779dd958d26a16b1a85
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-11-20 20:09:00 +0700

    add DormController

---
+++ Jonghor/Controllers/DormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jonghor.Controllers
+{
+    public class DormController : Controller
+    {
+        // GET: Dorm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+    }
+}
