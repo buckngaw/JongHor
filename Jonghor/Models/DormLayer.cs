185ec5113707b4641ee9414d8f1c2ff8c32d001b
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-12-14 21:03:12 +0700

    add DormDetailViewModel

--- Jonghor/Models/DormLayer.cs
+++ Jonghor/Models/DormLayer.cs
@@ -16,6 +16,36 @@ namespace Jonghor.Models
 
             return dormDal.ToList<Dorm>();
         }
+
+        public Dorm GetDorm(int id)
+        {
+            JongHorDBEntities1 jonghor = new JongHorDBEntities1();
+
+            IQueryable<Dorm> dorms = jonghor.Dorm.Where(d => d.Dorm_ID == id);
+            if (dorms.ToList<Dorm>().Count != 0)
+                return dorms.ToList<Dorm>()[0];
+            else
+            {
+                return new Dorm();
+            }
+        }
+
+        public List<Room_Type> GetRoomTypes(Dorm dorm)
+        {
+            JongHorDBEntities1 db = new JongHorDBEntities1();
+           
+            IQueryable<Room> rooms = db.Room.Where(r => r.Dorm_ID == dorm.Dorm_ID);
+            List<Room_Type> types = new List<Room_Type>();
+            foreach (Room room in rooms)
+            {
+                if(!types.Contains(room.Room_Type))
+                    types.Add(room.Room_Type);
+            }
+            
+            
+
+            return types;
+        }
     }
 
     
