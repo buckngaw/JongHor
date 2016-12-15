0bd3e7588ecbe801fafa05c6692223586c15e1f8
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-12-15 12:25:24 +0700

    done UserEditViewModel

--- Jonghor/ViewModel/UserProfileViewModel.cs
+++ Jonghor/ViewModel/UserProfileViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Jonghor.Models;
 
 namespace Jonghor.ViewModel
 {
@@ -10,5 +11,29 @@ namespace Jonghor.ViewModel
         public bool hasDorm { get; set; }
         public bool isRoomMateMode { get; set; }
         public int rate { get; set; }
+
+        public void SetUser(string name)
+        {
+            PersonBusinessLayer layer = new PersonBusinessLayer();
+            Person user = layer.GetUser(name);
+            isRoomMateMode = (Status)user.Room.Status == Status.WaitRoomMate;
+            hasDorm = user.Dorm_ID != null;
+            if (hasDorm)
+            {
+                var dormRate = user.Dorm_Rate.Where(u => u.Dorm_ID == user.Dorm_ID);
+                if (dormRate.Count() > 0)
+                {
+                    rate = (int)dormRate.ToList<Dorm_Rate>()[0].Score;
+                }
+                else
+                {
+                    rate = 0;
+                }
+            }
+            else
+            {
+                rate = 0;
+            }
+        }
     }
 }
