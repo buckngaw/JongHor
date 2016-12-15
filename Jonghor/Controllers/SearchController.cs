3f695321278420a73e6fa76f80b54a80521158a3
kaiyast <0kaiyast0@gmail.com>
2016-12-15 15:40:27 +0700

    Add controller to Dorm Detail

--- Jonghor/Controllers/SearchController.cs
+++ Jonghor/Controllers/SearchController.cs
@@ -195,8 +195,10 @@ namespace Jonghor.Controllers
 
         public ActionResult Dormdetail(int dorm)
         {
-
-            return View("Dormdetail");
+            DormDetailViewModel dormview = new DormDetailViewModel();
+            dormview.SetDorm(dorm);
+           
+            return View("Dormdetail", dormview);
         }
 
         //---------------------------------------------------------------------------
